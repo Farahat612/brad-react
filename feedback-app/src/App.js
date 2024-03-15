f00d54f6ecb601e90f1267baa3dea3825325cc4e
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-15 03:11:28 +0200

    rendered FeedbackList component in app.js

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedbacks={feedbacks} />
       </div>
     </>
   );
