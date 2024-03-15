7f967ae466589850fb4612d604fc07f8b524d9d2
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-15 23:28:38 +0200

    rendered Feedback Context provider

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -2,30 +2,34 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
+// import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 
-function App() {
-  const [feedbacks, setFeedbacks] = useState(FeedbackData)
-  const handleDelete = (id) => {
-    if (window.confirm('Are you sure you want to delete this feedback?')) {
-      setFeedbacks(feedbacks.filter((feedback) => feedback.id !== id))
-    }
-  }
+import { FeedbackProvider } from './contexts/FeedbackContext'
 
-  const handleAdd = (newFeedback) => {
-    setFeedbacks([...feedbacks, newFeedback])
-  }
+
+function App() {
+  // Commented all of these after using context API
+  // const [feedbacks, setFeedbacks] = useState(FeedbackData)
+  // const handleDelete = (id) => {
+  //   if (window.confirm('Are you sure you want to delete this feedback?')) {
+  //     setFeedbacks(feedbacks.filter((feedback) => feedback.id !== id))
+  //   }
+  // }
+
+  // const handleAdd = (newFeedback) => {
+  //   setFeedbacks([...feedbacks, newFeedback])
+  // }
   return (
-    <>
+    <FeedbackProvider>
       <Header />
       <div className='container'>
         <FeedbackForm addFeedback={handleAdd} />
         <FeedbackStats feedbacks={feedbacks} />
         <FeedbackList feedbacks={feedbacks} deleteFeedback={handleDelete} />
       </div>
-    </>
+    </FeedbackProvider>
   )
 }
 
