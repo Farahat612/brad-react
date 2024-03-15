d329a2b89d6bffea209ed08bdc3de12ee19986c5
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-15 19:03:42 +0200

    managed form state

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,10 +1,27 @@
-
+import { useState } from 'react'
+import Card from './shared/Card'
 
 const FeedbackForm = () => {
+  const [text, setText] = useState('')
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
   return (
-    <div>
-      Form
-    </div>
+    <Card>
+      <form>
+        <h2>How would you rate us?</h2>
+        {/* // Todo: rating select component */}
+        <div className='input-group'>
+          <input
+            value={text}
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Write a review'
+          />
+          <button type='submit'>Submit</button>
+        </div>
+      </form>
+    </Card>
   )
 }
 
