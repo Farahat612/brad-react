3670a758e1a683ee15a8fb557fe9874d7679fcfb
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-15 03:06:05 +0200

    created feedback data js

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 7,
+    text: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quae.',
+  },
+  {
+    id: 2,
+    rating: 8,
+    text: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quae.',
+  },
+  {
+    id: 3,
+    rating: 9,
+    text: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quae.',
+  },
+  {
+    id: 4,
+    rating: 10,
+    text: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quae.',
+  },
+]
+
+export default FeedbackData
