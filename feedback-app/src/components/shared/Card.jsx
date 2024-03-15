e76c62151da63e7d3a63b7603d30d68f5b338e80
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-15 03:26:02 +0200

    conditionally styled card

--- feedback-app/src/components/shared/Card.jsx
+++ feedback-app/src/components/shared/Card.jsx
@@ -1,5 +1,5 @@
-const Card = ({children}) => {
-  return <div className="card">
+const Card = ({children, reverse}) => {
+  return <div className={`card ${reverse && 'reverse'}`}>
     {children}
   </div>
 }
