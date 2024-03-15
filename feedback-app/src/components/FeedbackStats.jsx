46b318fdae816d01e5225c0825b3f33d7e984a77
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-15 04:53:10 +0200

    used RegEx to remove 0 decimal

--- feedback-app/src/components/FeedbackStats.jsx
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -1,16 +1,17 @@
 const FeedbackStats = ({ feedbacks }) => {
-
   //calculate ratings average
-  const ratingsAverage = feedbacks.reduce((total, feedback) => {
-      return total + feedback.rating 
+  const ratingsAverage =
+    feedbacks.reduce((total, feedback) => {
+      return total + feedback.rating
     }, 0) / feedbacks.length
-  
+
   return (
     <div className='feedback-stats'>
       <h4>{feedbacks.length} reviews</h4>
       <h4>
-        {isNaN(ratingsAverage) ? 0 : ratingsAverage.toFixed(1)} 
-        /10 stars average rating
+        {isNaN(ratingsAverage)
+          ? 0
+          : ratingsAverage.toFixed(1).replace(/[.,]0$/, '')}
       </h4>
     </div>
   )
