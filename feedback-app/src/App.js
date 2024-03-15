1a3a8270420e6cabf8751a9c956efe6d376b34fc
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-15 03:08:25 +0200

    created a state for feedback data in app.js

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import FeedbackItem from './components/FeedbackItem';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
+  const [feedbacks, setFeedbacks] = useState(FeedbackData);
   return (
     <>
       <Header />
