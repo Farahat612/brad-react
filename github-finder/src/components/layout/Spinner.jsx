9c608742e789829282df8cfcb4577fed7af22778
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-16 18:05:02 +0200

    created spinner component

---
+++ github-finder/src/components/layout/Spinner.jsx
@@ -0,0 +1,16 @@
+import spinnerImg from '../../assets/spinner.gif'
+
+const Spinner = () => {
+  return (
+    <div className='w-100 mt-20'>
+      <img
+        width={180}
+        className='text-center mx-auto'
+        src={spinnerImg}
+        alt='Loading...'
+      />
+    </div>
+  )
+}
+
+export default Spinner
