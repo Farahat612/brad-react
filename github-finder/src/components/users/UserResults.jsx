3d4e12774335511097796f4efd0558685d430507
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-16 18:01:31 +0200

    rendred the users result list

--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -1,24 +1,41 @@
-import {useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 const UserResults = () => {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const fetchUsers = async () => {
     const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
       method: 'GET',
       headers: {
-        'Authorization': `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-      }
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
     })
     const data = await response.json()
-    console.log(data)
+    setUsers(data)
+    setLoading(false)
   }
 
   useEffect(() => {
     fetchUsers()
   }, [])
+
   return (
-    <div>
-      User Results
+    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        users.map((user) => (
+          <div key={user.id} className='flex flex-col items-center'>
+            <img
+              src={user.avatar_url}
+              alt={user.login}
+              className='w-24 h-24 rounded-full'
+            />
+            <h2 className='mt-4 text-xl font-bold'>{user.login}</h2>
+          </div>
+        ))
+      )}
     </div>
   )
 }
