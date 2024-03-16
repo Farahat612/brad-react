2edbfa2d5a82c50507800789868373c28f85c866
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-16 18:06:10 +0200

    rendered spinner component

--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Spinner from '../layout/Spinner'
 
 const UserResults = () => {
   const [users, setUsers] = useState([])
@@ -23,7 +24,7 @@ const UserResults = () => {
   return (
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
       {loading ? (
-        <p>Loading...</p>
+        <Spinner />
       ) : (
         users.map((user) => (
           <div key={user.id} className='flex flex-col items-center'>
