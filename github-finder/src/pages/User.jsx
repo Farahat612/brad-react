e708ee460c582497f047cb34e59c57683c34e6b2
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-16 23:26:51 +0200

    used get user inside User component

--- github-finder/src/pages/User.jsx
+++ github-finder/src/pages/User.jsx
@@ -1,5 +1,15 @@
+import { useEffect, useContext } from 'react'
+import GitHubContext from '../contexts/github/GitHubContext'
+import { useParams } from 'react-router-dom'
+
 const User = () => {
-  return <div>User</div>
+  const { getUser, user} = useContext(GitHubContext)
+  const params = useParams()
+  useEffect(() => {
+    getUser(params.login)
+  }, [])
+
+  return <div>{user.login}</div>
 }
 
 export default User
