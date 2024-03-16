a82f36ca17bc4b7e61c6453a2c8747a4b0f14165
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-16 17:34:00 +0200

    setted routes and populated about and notfound pages with content

--- github-finder/src/pages/About.jsx
+++ github-finder/src/pages/About.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className='text-white'>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details. This
+        project is part of the
+        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
+          <strong> React Front To Back </strong>
+        </a>
+        Udemy course by
+        <strong>
+          <a href='https://traversymedia.com'> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Layout By :
+        <a className='text-white' href='https://twitter.com/hassibmoddasser'>
+          {' '}
+          Hassib Moddasser
+        </a>
+      </p>
     </div>
   )
 }
--- github-finder/src/pages/Home.jsx
+++ github-finder/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home
+      <h1 className="text-6xl">Welcome</h1>
     </div>
   )
 }
