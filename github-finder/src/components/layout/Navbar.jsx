da760d4c7bc3795ea8af70cd7eb39b69e125aa6d
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-16 17:06:51 +0200

    created the Navbar and initialized routing

---
+++ github-finder/src/components/layout/Navbar.jsx
@@ -0,0 +1,41 @@
+import { FaGithub } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+const Navbar = ({ title }) => {
+  return (
+    <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+      <div className='container mx-auto'>
+        <div className='flex-none px-2 mx-2'>
+          <Link to='/'>
+            <FaGithub className='inline-block pr-2 text-3xl' />
+            <span className='text-lg align-middle font-bold'>{title}</span>
+          </Link>
+        </div>
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
+            <Link className='btn btn-ghost btn-sm rounded-btn uppercase' to='/'>
+              Home
+            </Link>
+            <Link
+              className='btn btn-ghost btn-sm rounded-btn uppercase'
+              to='/about'
+            >
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+}
+
+export default Navbar
