08d565d64b2479037ecd8724952429a6d5848029
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-20 20:58:30 +0200

    Example Done

--- custom-hooks/src/components/CustomHookExample1.jsx
+++ custom-hooks/src/components/CustomHookExample1.jsx
@@ -1,7 +1,22 @@
 import useFetch from '../hooks/useFetch'
 
 const CustomHookExample1 = () => {
-  return <div>useFetch Example</div>
+  const { data, loading, error } = useFetch(
+    'https://jsonplaceholder.typicode.com/posts',
+    {}
+  )
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Error: {error.message}</div>
+  return (
+    <div>
+      {data.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default CustomHookExample1
