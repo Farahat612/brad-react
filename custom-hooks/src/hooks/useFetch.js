67ad1820e7a14b4a45451429b0bb3a6619c0bfec
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-20 20:50:47 +0200

    useFetch finalr

--- custom-hooks/src/hooks/useFetch.js
+++ custom-hooks/src/hooks/useFetch.js
@@ -1,11 +1,25 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react'
 
-function useFetch() {
+function useFetch(url, options) {
   const [loading, setLoading] = useState(true)
   const [data, setData] = useState(null)
   const [error, setError] = useState(null)
 
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url, options)
+        const jsonData = await res.json()
+        setData(jsonData)
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
 
   return { data, loading, error }
 }
