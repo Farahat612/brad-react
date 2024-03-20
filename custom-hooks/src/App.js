cdfd366d21335f44c605a51848af4e2e19116bff
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-20 20:56:07 +0200

    rebuilt the component

--- custom-hooks/src/App.js
+++ custom-hooks/src/App.js
@@ -1,7 +1,10 @@
+import CustomHookExample1 from './components/CustomHookExample1'
+
 function App() {
   return (
     <div className='App'>
       <h1>Custom Hooks</h1>
+      <CustomHookExample1 />
     </div>
   )
 }
---
+++ custom-hooks/src/components/CustomHookExample1.jsx
@@ -0,0 +1,7 @@
+import useFetch from '../hooks/useFetch'
+
+const CustomHookExample1 = () => {
+  return <div>useFetch Example</div>
+}
+
+export default CustomHookExample1
