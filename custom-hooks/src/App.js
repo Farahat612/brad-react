3623f4b78de7ad6ca3072adcbf0aee861f83bc7c
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-20 20:53:42 +0200

    removed the component to fix camelcase issue

--- custom-hooks/src/App.js
+++ custom-hooks/src/App.js
@@ -1,11 +1,9 @@
-
-
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Custom Hooks</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
