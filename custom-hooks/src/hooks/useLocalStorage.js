97d37ae828b34a22087809b99213e259f42b82ce
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-20 21:08:47 +0200

    second hook first part

--- custom-hooks/src/hooks/useLocalStorage.js
+++ custom-hooks/src/hooks/useLocalStorage.js
@@ -1,8 +1,18 @@
 import { useState } from 'react'
 
-const useLocalStorage = () => {
-  const [value, setValue] = useState()
- 
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => getLocalStorageValue(key, initialValue))
 }
 
+
+
+const getLocalStorageValue = (key, initialValue) => {
+  try {
+    const item = window.localStorage.getItem(key)
+    return item ? JSON.parse(item) : initialValue
+  } catch (error) {
+    console.error(error)
+    return initialValue
+  }
+}
 export default useLocalStorage
