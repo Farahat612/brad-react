e720312e3580f390b2e809b9236a540cf782172a
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-20 21:24:02 +0200

    second hook example done

--- custom-hooks/src/components/CustomHookExample2.jsx
+++ custom-hooks/src/components/CustomHookExample2.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
+import useLocalStorage from '../hooks/useLocalStorage'
 
 const CustomHookExample2 = () => {
+  const [tasks, setTasks] = useLocalStorage('task', [])
+  const [task, setTask] = useState('')
+
+  const addTask = (e) => {
+    e.preventDefault()
+    if (task.trim() === '') return
+    setTasks([...tasks, task])
+    setTask('')
+  }
   return (
     <div>
-      setLocalStorage Example
+      <form>
+        <input
+          type='text'
+          placeholder='Enter a task'
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type='submit' onClick={addTask}>
+          Add Task
+        </button>
+      </form>
     </div>
   )
 }
