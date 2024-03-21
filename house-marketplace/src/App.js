9405ab9b6e693729889f9a0a0f203df193cdc9e5
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-21 04:13:17 +0200

    created privateRoute and spinner components

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -10,6 +10,7 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import ForgotPassword from './pages/ForgotPassword'
 import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
@@ -18,7 +19,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoute />} >
+            <Route index element={<Profile />} />
+          </Route>
           <Route path='/sign-in' element={<Signin />} />
           <Route path='/sign-up' element={<Signup />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
