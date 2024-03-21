49ed6cd8ab4434cec54c0294516e7eeb019524d2
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-21 02:42:24 +0200

    used toastify for alerts and displayed user name in profile

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
@@ -24,7 +26,8 @@ function App() {
         </Routes>
         <Navbar />
       </Router>
-     </>
+      <ToastContainer />
+    </>
   )
 }
 
--- house-marketplace/src/pages/Profile.jsx
+++ house-marketplace/src/pages/Profile.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react"
+import { getAuth } from "firebase/auth"
+
 const Profile = () => {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  )
+  const [user, setUser] = useState(null)
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+  return user ? <h1>{user.displayName}</h1> : <h1>not logged in</h1>
 }
 
 export default Profile
