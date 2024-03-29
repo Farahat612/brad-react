fec6129adb7d5675bad85c8348981a6c4452b313
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-29 02:35:28 +0200

    created Listing Page Route and displayed initial listing details

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -11,6 +11,7 @@ import Signup from './pages/Signup'
 import ForgotPassword from './pages/ForgotPassword'
 import Category from './pages/Category'
 import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
 
 import Navbar from './components/Navbar'
 import PrivateRoute from './components/PrivateRoute'
@@ -30,6 +31,10 @@ function App() {
           <Route path='/sign-up' element={<Signup />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
           <Route path='*' element={<NotFound />} />
         </Routes>
         <Navbar />
