5f1fab15c8936f9812d79a4b7fa5cabe5f003c91
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-03-29 02:39:50 +0200

    created Contact Page Route

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -12,6 +12,7 @@ import ForgotPassword from './pages/ForgotPassword'
 import Category from './pages/Category'
 import CreateListing from './pages/CreateListing'
 import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 import Navbar from './components/Navbar'
 import PrivateRoute from './components/PrivateRoute'
@@ -35,6 +36,7 @@ function App() {
             path='/category/:categoryName/:listingId'
             element={<Listing />}
           />
+          <Route path='/contact/:landlordID' element={<Contact />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
         <Navbar />
