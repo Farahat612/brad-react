2d4ce13c4ad28f481be4cab39ca8dc77600b7a51
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-01 23:23:05 +0200

    connected to db from server and organized the code

--- support-desk/backend/server.js
+++ support-desk/backend/server.js
@@ -2,22 +2,29 @@ const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
+// Connect to MongoDB
+connectDB()
+
+// Initialize Express
 const app = express()
+
+// Middleware
+// Body Parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+// Error Handler Middleware
+app.use(errorHandler)
 
+// Routes
 // Home Route
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the Spport Desk system' })
 })
-
-// Routes
+// User Routes
 app.use('/api/users', require('./routes/userRoutes'))
-
-// Error Handler Middleware
-app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
