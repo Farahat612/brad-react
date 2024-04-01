0ca2ef733001c60c3db851e6452d936a166a3f2d
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-01 23:56:56 +0200

    connected User model

---
+++ support-desk/backend/modles/userModle.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide a name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide an email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+    },
+    isAdmin: {
+      type: Boolean,
+      required: [true, 'Please provide a role'],
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('User', userSchema)
