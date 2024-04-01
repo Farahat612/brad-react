e225e2f8e904b6ddaecf2d02b4b1d1f6c4525636
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-01 22:28:30 +0200

    documented controllers using comments

--- support-desk/backend/controllers/userController.js
+++ support-desk/backend/controllers/userController.js
@@ -1,7 +1,13 @@
+// @desc: Register a new user
+// @route: POST /api/users
+// @access: Public
 const registerUser = (req, res) => {
   res.json({ message: 'Register Route' })
 }
 
+// @desc: Login a user
+// @route: POST /api/users/login
+// @access: Public
 const loginUser = (req, res) => {
   res.json({ message: 'Login Route' })
 }
