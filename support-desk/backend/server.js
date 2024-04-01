de11b5c6877a53b0c13b6750dbf7e1475394bf20
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-01 21:49:51 +0200

    initialized the server

--- support-desk/backend/server.js
+++ support-desk/backend/server.js
@@ -1,1 +1,8 @@
-console.log('Just server ...')
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
