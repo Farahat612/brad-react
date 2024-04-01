a3d672279ad956bc43469d0cf61947746c4d22d9
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-01 22:34:03 +0200

    parsed body json in server

--- support-desk/backend/server.js
+++ support-desk/backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the Spport Desk system' })
