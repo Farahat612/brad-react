4ef78cbc952d725c53185b9663d0283f0cc39b7b
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-02 02:12:51 +0200

    Register Page first part

--- support-desk/frontend/src/pages/Register.jsx
+++ support-desk/frontend/src/pages/Register.jsx
@@ -1,8 +1,51 @@
+import { useState } from 'react'
+import { FaUser } from 'react-icons/fa'
+
 const Register = () => {
+  // Creating a state variable to store the form data
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+
+  // Destructuring the form data
+  const { name, email, password, password2 } = formData
+
+  // Creating a function to handle form data
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className='form'>
+        <form>
+          <div className='form-group'>
+            <input
+              className='form-control'
+              type='text'
+              placeholder='Enter your name'
+              id='name'
+              name='name'
+              value={name}
+              onChange={onChange}
+            />
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
