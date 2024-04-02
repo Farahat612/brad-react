d62adcd9230ac1b30e2f238a8442d531783ce616
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-02 02:00:05 +0200

    created three routes for pages in app.js

--- support-desk/frontend/src/App.js
+++ support-desk/frontend/src/App.js
@@ -1,8 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 const App = () => {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container '>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   )
 }
 
