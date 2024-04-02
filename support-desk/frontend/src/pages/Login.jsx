c0ac219cc8a34cf16cc406a787d262589e90cafb
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-02 02:25:18 +0200

    Login initial

--- support-desk/frontend/src/pages/Login.jsx
+++ support-desk/frontend/src/pages/Login.jsx
@@ -1,8 +1,71 @@
+import { useState } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+
 const Login = () => {
+  // Creating a state variable to store the form data
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  // Destructuring the form data
+  const { email, password } = formData
+
+  // Creating a function to handle form data
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  // Creating a function to handle form submission
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please log in to get support</p>
+      </section>
+
+      <section className='form' onSubmit={onSubmit}>
+        <form>
+          <div className='form-group'>
+            <input
+              className='form-control'
+              type='email'
+              placeholder='Enter your email'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              className='form-control'
+              type='password'
+              placeholder='Enter your password'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Login</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
