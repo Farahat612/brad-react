2454ad47a3363688b8de2cbc0dff1661b1ad83e7
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-04 01:56:12 +0200

    created notes router and connected to ticket router

--- support-desk/backend/routes/ticketRoutes.js
+++ support-desk/backend/routes/ticketRoutes.js
@@ -10,6 +10,10 @@ const {
   deleteTicket,
 } = require('../controllers/ticketController')
 
+// Re-route into note router
+const noteRouter = require('./noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
+
 // Tickets Routes
 router.route('/').get(protect, getTickets).post(protect, createTicket)
 router
