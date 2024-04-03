d8e47866158109a3493e94cbc24482466c91e6f3
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-03 21:18:13 +0200

    PrivateRoute done

--- support-desk/frontend/src/components/PrivateRoute.jsx
+++ support-desk/frontend/src/components/PrivateRoute.jsx
@@ -1,9 +1,16 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
+
 const PrivateRoute = () => {
-  return (
-    <div>
-      <h1>Private Route</h1>
-    </div>
-  )
+  // Get the authentication status and checking status
+  const { isAuthenticated, checkingStatus } = useAuthStatus()
+  // Show a spinner while checking the authentication status
+  if (checkingStatus) {
+    return <Spinner />
+  }
+  // Redirect to the login page if the user is not authenticated
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
 }
 
 export default PrivateRoute
