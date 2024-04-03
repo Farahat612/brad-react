ad0873ae0c82aa6cb17c3cd426859ebe1a50e523
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-03 21:53:26 +0200

    Created createTicket Thunk in ticket Slice

--- support-desk/frontend/src/features/tickets/ticketSlice.js
+++ support-desk/frontend/src/features/tickets/ticketSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-// import ticketService from '../../services/ticketService'
+import ticketService from '../../services/ticketService'
 
+// Defining the initial state
 const initialState = {
   tickets: [],
   ticket: {},
@@ -10,6 +11,28 @@ const initialState = {
   message: '',
 }
 
+// Creating an async thunk to handle ticket creation
+export const createTicket = createAsyncThunk(
+  'ticket/create',
+  // Defining the action to be dispatched
+  async (ticketData, thunkAPI) => {
+    try {
+      // Getting the user token from the state
+      const token = thunkAPI.getState().auth.user.token
+      // Dispatching the createTicket action from the ticket service
+      return await ticketService.createTicket(ticketData, token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
 export const ticketSlice = createSlice({
   name: 'ticket',
   initialState,
@@ -18,20 +41,20 @@ export const ticketSlice = createSlice({
   },
   extraReducers: (builder) => {
     // builder
-      // .addCase(createTicket.pending, (state) => {
-      //   state.isLoading = true
-      // })
-      // .addCase(createTicket.fulfilled, (state, action) => {
-      //   state.isLoading = false
-      //   state.isSuccess = true
-      //   state.message = action.payload.message
-      // })
-      // .addCase(createTicket.rejected, (state, action) => {
-      //   state.isLoading = false
-      //   state.isErrors = true
-      //   state.message = action.error.message
-      // })
-  }
+    // .addCase(createTicket.pending, (state) => {
+    //   state.isLoading = true
+    // })
+    // .addCase(createTicket.fulfilled, (state, action) => {
+    //   state.isLoading = false
+    //   state.isSuccess = true
+    //   state.message = action.payload.message
+    // })
+    // .addCase(createTicket.rejected, (state, action) => {
+    //   state.isLoading = false
+    //   state.isErrors = true
+    //   state.message = action.error.message
+    // })
+  },
 })
 
 export const { reset } = ticketSlice.actions
