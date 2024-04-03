599ab4fc540ed9a41812e82039e57a445bc751ee
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-04 01:21:00 +0200

    Ticket page rendered ticket data

--- support-desk/frontend/src/pages/Ticket.jsx
+++ support-desk/frontend/src/pages/Ticket.jsx
@@ -31,9 +31,31 @@ const Ticket = () => {
     }
   }, [dispatch, ticketId, isError, message])
 
+  // Displaying a spinner while the ticket is being fetched
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  // Displaying an error message if the ticket fetching fails
+  if (isError) {
+    return <h3>Something went wrong!</h3>
+  }
   return (
-    <div>
-      <h1>Ticket</h1>
+    <div className='ticket-page'>
+      <header className='ticket-header'>
+        <BackButton url='/tickets' />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>Date Submitted :{new Date(ticket.createdAt).toLocaleString()}</h3>
+        <div className='ticket-desc'>
+          <h3>Description of Issue</h3>
+          <p> {ticket.description} </p>
+        </div>
+      </header>
     </div>
   )
 }
