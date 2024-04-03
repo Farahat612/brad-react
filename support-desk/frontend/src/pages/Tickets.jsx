03d5cf20c8371e188aaafd2bac806c72310a7e97
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-03 22:48:22 +0200

    Tickets Page initial

--- support-desk/frontend/src/pages/Tickets.jsx
+++ support-desk/frontend/src/pages/Tickets.jsx
@@ -5,6 +5,31 @@ import Spinner from '../components/Spinner'
 import BackButton from '../components/BackButton'
 
 const Tickets = () => {
+  // Getting the ticket state from the store
+  const { tickets, isLoading, isSuccess } = useSelector((state) => state.ticket)
+
+  // Initializing the dispatch
+  const dispatch = useDispatch()
+
+  // Resetting the ticket state when the component unmounts
+  useEffect(() => {
+    return () => {
+      if (isSuccess) {
+        dispatch(reset())
+      }
+    }
+  }, [dispatch, isSuccess])
+
+  // Fetching the tickets when the component mounts
+  useEffect(() => {
+    dispatch(getTickets())
+  }, [dispatch])
+
+  // Displaying the loading spinner while fetching the tickets
+  if (isLoading) {
+    return <Spinner />
+  }
+
   return (
     <div>
       <h1>Tickets</h1>
