208e9a706d8a3fd4f7b29b15d34e8f716ae05205
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-04 01:12:58 +0200

    Ticket page initial

--- support-desk/frontend/src/pages/Ticket.jsx
+++ support-desk/frontend/src/pages/Ticket.jsx
@@ -1,4 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { getTicketById, reset } from '../features/tickets/ticketSlice'
+
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+
 const Ticket = () => {
+  // Accessing the ticket state from the store
+  const { ticket, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.tickets
+  )
+
+  // Getting the dispatch function from the useDispatch hook
+  const dispatch = useDispatch()
+
+  // Getting the ticketId from the URL params
+  const { ticketId } = useParams()
+
+  // Fetching the ticket by the ticketId when the component mounts
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    dispatch(getTicketById(ticketId))
+    // Resetting the ticket state when the component unmounts
+    return () => {
+      dispatch(reset())
+    }
+  }, [dispatch, ticketId, isError, message])
+
   return (
     <div>
       <h1>Ticket</h1>
