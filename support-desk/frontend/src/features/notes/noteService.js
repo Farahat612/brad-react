dadcb01d8c052c386527c5df870668789a47be2f
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-04 19:39:38 +0200

    Created createNote Service

--- support-desk/frontend/src/features/notes/noteService.js
+++ support-desk/frontend/src/features/notes/noteService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Defining API base URL
 const API_URL = '/api/tickets/'
 
-// Getting all tickets
+// Getting all Notes for a ticket
 const getNotes = async (ticketId, token) => {
   // Setting the request headers with the user token
   const config = {
@@ -15,9 +15,23 @@ const getNotes = async (ticketId, token) => {
   return response.data
 }
 
+// Creating a Note for a ticket
+const createNote = async (ticketId, note, token) => {
+  // Setting the request headers with the user token
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  const response = await axios.post(`${API_URL}${ticketId}/notes`, note, config)
+  return response.data
+}
+
 // Creating an object to hold the ticket service functions
 const noteService = {
   getNotes,
+  createNote,
 }
 
 // Exporting the ticket service object
