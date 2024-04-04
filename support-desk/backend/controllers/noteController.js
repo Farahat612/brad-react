3c614808a6c53848dc092ce42fdbd6a080d35250
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-04-04 02:20:48 +0200

    created createtNote in noteController

--- support-desk/backend/controllers/noteController.js
+++ support-desk/backend/controllers/noteController.js
@@ -40,4 +40,48 @@ const getNotes = asyncHandler(async (req, res) => {
   res.json(notes)
 })
 
-module.exports = { getNotes }
+// @desc    Create a new note
+// @route   POST /api/tickets/:ticketId/notes
+// @access  Private
+const createNote = asyncHandler(async (req, res) => {
+  // Get the logged in user id in the JWT
+  const user = await User.findById(req.user.id)
+  // Check if the user exists
+  if (!user) {
+    res.status(404)
+    throw new Error('User not found')
+  }
+  // Check if the ticket ID is valid
+  if (!mongoose.Types.ObjectId.isValid(req.params.ticketId)) {
+    res.status(400)
+    throw new Error('Invalid ticket ID')
+  }
+  // Get the ticket by ID
+  const ticket = await Ticket.findById(req.params.ticketId)
+  // Check if the ticket doesn't exist
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+  // Validate if the ticket belongs to the logged in user
+  if (ticket.user.toString() !== req.user._id.toString()) {
+    res.status(401)
+    throw new Error('Not authorized to view this ticket')
+  }
+
+  // Get the content, isStaff, and staffId from the request body
+  const { content, isStaff, staffId } = req.body
+
+  // Create a new note
+  const note = await Note.create({
+    user: req.user._id,
+    ticket: req.params.ticketId,
+    content,
+    isStaff,
+    staffId,
+  })
+
+  res.status(201).json(note)
+})
+
+module.exports = { getNotes, createNote }
